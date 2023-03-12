9ac3382cc7aa07f9500f8ddf173ce738f995356c
Olivier Gamache <olivier@glo.quebec>
2023-03-12 19:23:04 -0400

    chore: justfile and tailwindcss

---
+++ justfile
@@ -0,0 +1,5 @@
+watch-tailwind:
+  pnpx tailwindcss -i ./style/input.css -o ./style/main.css --watch
+
+watch-leptos:
+  cargo leptos watch
--- src/app.rs
+++ src/app.rs
@@ -4,20 +4,12 @@ use leptos_router::*;
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
-    // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context(cx);
 
     view! {
         cx,
-
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/start-axum.css"/>
-
-        // sets the document title
         <Title text="Welcome to Leptos"/>
-
-        // content for this welcome page
         <Router>
             <main>
                 <Routes>
@@ -31,12 +23,16 @@ pub fn App(cx: Scope) -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage(cx: Scope) -> impl IntoView {
-    // Creates a reactive value to update the button
     let (count, set_count) = create_signal(cx, 0);
     let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! { cx,
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <div class="m-10">
+            <h1 class="text-3xl">"Welcome to Leptos!"</h1>
+            <div class="flex items-center">
+                <button class="m-5 p-2 bg-red-500 rounded-lg hover:scale-105 transition-transform" on:click=on_click>"Click Me !"</button>
+                <p class="text-xl">{count}</p>
+            </div>
+        </div>
     }
 }
